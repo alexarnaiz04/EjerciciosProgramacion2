8a912f0049f7b3c83d669e981ee4c6633e22b2df
alexarnaiz04 <115986338+alexarnaiz04@users.noreply.github.com>
2024-02-22 14:17:41 +0100

    Add files via upload

---
+++ Ejercicio5/DesbordamientoPila.java
@@ -0,0 +1,5 @@
+/*
+El desbordamiento de pila sucede cuando hay demasiadas llamadas recursivas, lo que causa que la memoria reservada para el stack de llamadas se llene por completo.
+Si las llamadas recursivas son excesivas, la pila alcanza su límite y se produce un desbordamiento.
+A pesar de esto, en ocasiones una solución recursiva puede resultar más clara y fácil de entender que su contraparte no recursiva. En estos escenarios, puede ser válido sacrificar un poco de eficiencia en términos de memoria para mejorar la legibilidad del código.
+*/
