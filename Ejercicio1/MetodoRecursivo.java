a984b2b84eab4a575d69f8386589924927281ef7
alexarnaiz04 <115986338+alexarnaiz04@users.noreply.github.com>
2024-02-22 12:42:59 +0100

    Add files via upload

---
+++ Ejercicio1/MetodoRecursivo.java
@@ -0,0 +1,5 @@
+/*
+Una solución recursiva define la solución a un problema resolviendo problemas similares. 
+En programación esto se traduce en subprogramas que se invocan a si mismos.
+Podemos diferenciar dos tipos de algoritmos recursivos: con recursividad directa (A ≡f(A)) e recursividad indirecta (A ≡f(B) y B ≡f(A))
+*/
