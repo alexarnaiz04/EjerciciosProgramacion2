60012d9d2c784ffdf4e68b1804d23f00631646ae
alexarnaiz04 <115986338+alexarnaiz04@users.noreply.github.com>
2024-03-03 23:11:53 +0100

    Add files via upload

---
+++ Ejercicio12/expresionLambda.java
@@ -0,0 +1,18 @@
+/*
+ * Las expresiones lambda en Java son una forma de definir la correspondencia entre ciertos argumentos y un resultado. Básicamente, nos permiten establecer reglas de cómo convertir 
+ * unos valores de entrada en un valor de salida. Estas expresiones lambda se utilizan para definir funciones o subrutinas de manera anónima, lo que significa que podemos crear 
+ * funciones sin darles un nombre explícito.
+ * 
+ * Existen varias razones para utilizar expresiones lambda:
+ * 
+ * Gran Potencia y Expresividad: Las expresiones lambda son una herramienta poderosa que nos permite expresar lógica de una manera concisa y fácil de entender. Nos permiten escribir 
+ * código de manera más elegante y compacta.
+ * 
+ * Simplicidad y Reducción de Código: Las expresiones lambda son un mecanismo simple y potente para definir comportamientos o acciones que no necesitan ser reutilizados en múltiples 
+ * lugares. Esto reduce notablemente la cantidad de código que necesitamos escribir, ya que podemos definir la función directamente en el lugar donde la necesitamos, sin necesidad 
+ * de declarar una clase o método separado.
+ * 
+ * Argumentos de Funciones de Orden Superior: Las expresiones lambda se utilizan frecuentemente como argumentos que se pasan a funciones de orden superior. Por ejemplo, pueden ser 
+ * utilizadas como comparadores para ordenar un contenedor de elementos. Esto significa que podemos pasar comportamientos como parámetros a otras funciones, lo que hace que nuestro 
+ * código sea más flexible y modular.
+ */
